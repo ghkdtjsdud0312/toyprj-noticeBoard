9ebcdc3a1462cc335db4f2d258a20d6f29eb9062
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-05-29 22:23:33 +0900

    Initial commit

---
+++ src/main/java/toyprj/toyprj_noticeBoard/ToyprjNoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package toyprj.toyprj_noticeBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyprjNoticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyprjNoticeBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toyprj/toyprj_noticeBoard/ToyprjNoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package toyprj.toyprj_noticeBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyprjNoticeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
