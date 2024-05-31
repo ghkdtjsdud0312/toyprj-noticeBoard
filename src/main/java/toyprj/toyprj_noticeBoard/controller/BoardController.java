d4058fe50ae364a284a0dbe760354f4332d7f2c9
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-06-01 01:29:45 +0900

    6/1 boardController 수정

--- src/main/java/toyprj/toyprj_noticeBoard/controller/BoardController.java
+++ src/main/java/toyprj/toyprj_noticeBoard/controller/BoardController.java
@@ -24,6 +24,6 @@ public class BoardController {
     // ModelAttribute 로테이션에 의해 BoardDTO의 boardDTO 객체를 찾아서 html의 name과 dto의 이름이 동일하다면 스프링이 해당하는 필드에 대한 setter를 호출하면서 dto에 담겨진 값을 setter method로 각각 담아준다.
     public String save(@ModelAttribute BoardDTO boardDTO) { // @RequestParam은 html의 값을 불러오기 위해서 사용하는 것인데 DTO를 세팅해주면 일일이 RequestParam 받아 올 필요 없이 DTO 하나로 받으면 된다.
         boardService.save(boardDTO);
-        return null;
+        return "index";
     }
 }
