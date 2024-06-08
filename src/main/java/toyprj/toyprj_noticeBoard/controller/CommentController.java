800571157cc5458b031387d03d9b8ed999bd5e3e
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-06-08 23:31:25 +0900

    6/8 repository, comtroller 주입 추가

--- src/main/java/toyprj/toyprj_noticeBoard/controller/CommentController.java
+++ src/main/java/toyprj/toyprj_noticeBoard/controller/CommentController.java
@@ -7,11 +7,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import toyprj.toyprj_noticeBoard.dto.CommentDTO;
+import toyprj.toyprj_noticeBoard.service.CommentService;
 
 @Controller
 @RequestMapping("/comment")
 @RequiredArgsConstructor
 public class CommentController {
+
+    // commentService 주입
+    private final CommentService commentService;
+
     @PostMapping("/save")
     public @ResponseBody String save(@ModelAttribute CommentDTO commentDTO) {
         System.out.println("commentDTO = " + commentDTO);
--- src/main/java/toyprj/toyprj_noticeBoard/service/CommentService.java
+++ src/main/java/toyprj/toyprj_noticeBoard/service/CommentService.java
@@ -2,9 +2,11 @@ package toyprj.toyprj_noticeBoard.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import toyprj.toyprj_noticeBoard.repository.CommentRepository;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
-
+    // repository 주입 받기
+    private final CommentRepository commentRepository;
 }
