d4d2a07b4ec9ee95b277d84c272488ec50f38a9b
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-06-08 23:23:46 +0900

    6/8 CommentEntity 내용추가 및 CommentService 파일 추가

---
+++ src/main/java/toyprj/toyprj_noticeBoard/entity/CommentEntity.java
@@ -0,0 +1,28 @@
+package toyprj.toyprj_noticeBoard.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "comment_table")
+public class CommentEntity extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String commentWriter;
+
+    @Column
+    private String commentContents;
+
+    // Board: Comment = 1:N
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private BoardEntity boardEntity;
+
+}
---
+++ src/main/java/toyprj/toyprj_noticeBoard/service/CommentService.java
@@ -0,0 +1,10 @@
+package toyprj.toyprj_noticeBoard.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+}
