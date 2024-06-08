55b41e093da4c7b4d0dea5755ab1b13bd4da4e86
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-06-08 22:45:30 +0900

    6/8 commentDTO 파일 추가

---
+++ src/main/java/toyprj/toyprj_noticeBoard/dto/CommentDTO.java
@@ -0,0 +1,18 @@
+package toyprj.toyprj_noticeBoard.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class CommentDTO {
+    private Long id; // 댓글 번호
+    private String commentWriter; // 작성자
+    private String commentContents; // 작성 내용
+    private Long boardId; // 게시물 번호(fk)
+    private LocalDateTime commentSavedTime; // 댓글 작성 시간
+}
