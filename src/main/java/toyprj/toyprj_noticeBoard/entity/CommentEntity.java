f5f2b701a6c59f5ae7f8d4bae80416c1bfa45447
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-06-09 22:21:47 +0900

    6/9 service,entity 내용추가

--- src/main/java/toyprj/toyprj_noticeBoard/entity/CommentEntity.java
+++ src/main/java/toyprj/toyprj_noticeBoard/entity/CommentEntity.java
@@ -26,7 +26,12 @@ public class CommentEntity extends BaseEntity{
     @JoinColumn(name = "board_id")
     private BoardEntity boardEntity;
 
-    public static void toSaveEntity(CommentDTO commentDTO) {
-
+    public static CommentEntity toSaveEntity(CommentDTO commentDTO, BoardEntity boardEntity) {
+        // class 내부이기 때문에 사용
+        CommentEntity commentEntity = new CommentEntity();
+        commentEntity.setCommentWriter(commentDTO.getCommentWriter());
+        commentEntity.setCommentContents(commentDTO.getCommentContents());
+        commentEntity.setBoardEntity(boardEntity);
+        return commentEntity;
     }
 }
--- src/main/java/toyprj/toyprj_noticeBoard/service/CommentService.java
+++ src/main/java/toyprj/toyprj_noticeBoard/service/CommentService.java
@@ -3,17 +3,31 @@ package toyprj.toyprj_noticeBoard.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import toyprj.toyprj_noticeBoard.dto.CommentDTO;
+import toyprj.toyprj_noticeBoard.entity.BoardEntity;
 import toyprj.toyprj_noticeBoard.entity.CommentEntity;
+import toyprj.toyprj_noticeBoard.repository.BoardRepository;
 import toyprj.toyprj_noticeBoard.repository.CommentRepository;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
     // repository 주입 받기
     private final CommentRepository commentRepository;
+    // 부모 엔티티 받기
+    private final BoardRepository boardRepository;
 
-    public void save(CommentDTO commentDTO) {
-        // Entity -> DTO 변환
-        CommentEntity.toSaveEntity(commentDTO);
+    public Long save(CommentDTO commentDTO) {
+        // 부모 엔티티(BoardEntity) 조회
+        Optional<BoardEntity> optionalBoardEntity = boardRepository.findById(commentDTO.getBoardId());
+        if (optionalBoardEntity.isPresent()) {
+            BoardEntity boardEntity = optionalBoardEntity.get();
+            // Entity -> DTO 변환
+            CommentEntity commentEntity = CommentEntity.toSaveEntity(commentDTO,boardEntity);
+            return commentRepository.save(commentEntity).getId();
+        } else {
+            return null;
+        }
     }
 }
